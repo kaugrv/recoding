55cf8dfe8249d7aa23c626813064aa418e34e9cb
Wendy Gervais <rubykiara1712@gmail.com>
2022-03-07 12:34:58 +0100

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(mouseX-2, mouseY-2, 50);
+  ellipse(mouseX-2, mouseY-2, 50);
+  // CC
+  // <3
+}
