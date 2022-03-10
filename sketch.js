250fa5241dfce5d296a7360dccfbc9d9d35224a7
Wendy <rubykiara1712@gmail.com>
2022-03-10 14:23:17 +0100

    V1.1 english translation + pseudo-code

--- sketch.js
+++ sketch.js
@@ -1,30 +1,38 @@
+// Recoding project 2022
+// T. DEBEAUNE et W. GERVAIS
+// Vera Molnar - Square Stuctures
+
+// Static Version (V1)
+
 function setup() {
-  createCanvas(700, 700);
+  let img = createCanvas(700, 700);
   background(220);
-  TraceMaille(5, 10,5);
+  scale(1.5,1.5,0);
+  DrawMesh(3,6,6);
+  
+  //saveCanvas('img', 'png');
+  
+
 }
 
-function draw() {
-}
-
-// Trace n quadrilatères aléatoires superposés, basés sur carrés 40*40
-// en colonne i, ligne j
-function TraceCarre(n, i, j) {
+// Draw n superposed quadrangles (based on 40*40 squares)
+// position i-column, j-line in the Mesg
+function DrawSquare(n, i, j) {
   push(); 
     noFill();
-    translate(60*i, 60*j, 0); // place en colonne i, ligne j, écarts de 20 entre les quadrilatères
+    translate(55*i, 55*j, 0); // put it on i-column, j-column, with a 20px distance between 2 quads
 
     for (let k=0; k<n; k++) { 
       let dispersion = [];
 
-      for (let d=0; d<8; d++) { // on génère "dispersion", un tableau de 8 entiers relatifs aléatoires = les 8 dispersions des coordonnées du quadrilatère
+      for (let d=0; d<8; d++) { // generate the dispersion array, filled with 8 random relative integers which will be the dispersions around the 8 coordinates
         let disp = int (random(-10,10));
         dispersion.push(disp);
       }
       
-      stroke(20*i,20*j,20*(i+j)); // dégradé de couleurs
+      stroke(20*(i+j),20*i,20*j); // color gradient
       
-// on trace le quadrilatère :
+// draw the quadrangle :
       quad(40+dispersion[0],40+dispersion[1],80+dispersion[2],40+dispersion[3],80+dispersion[4],80+dispersion[5],40+dispersion[6],80+dispersion[7]);
 
     }
@@ -32,11 +40,12 @@ function TraceCarre(n, i, j) {
   pop();
 }
 
-// Trace une maille de w*h quadrilatères aléatoires (superposition de n)
-function TraceMaille(n,w,h) {
+// Draw the i*j mesh of n superposed quadrangles
+function DrawMesh(n,w,h) {
   for (let i=0; i<w; i++) {
-    for (let j=0; j<h; j++) {
-        TraceCarre(n,i,j);
+    for (let j=0; j<h/2; j++) {
+        DrawSquare(2*(i+j)+2,i,j);
+        DrawSquare(2*(i+j)+2,w-1-i, h-1-j);
     }
   }
   
