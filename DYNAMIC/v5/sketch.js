0197563e134143331528467f1ca88fec4a50157c
Wendy Gervais <rubykiara1712@gmail.com>
2022-03-21 21:08:03 +0100

    Dynamic v5

---
+++ DYNAMIC/v5/sketch.js
@@ -0,0 +1,106 @@
+// Recoding project 2022
+// T. DEBEAUNE et W. GERVAIS
+// Vera Molnar - Square Structures
+
+// DYNAMIC Version (V1)
+let s=1;
+
+function setup() {
+  let img = createCanvas(700, 700);
+  //saveCanvas('img', 'png');
+  
+}
+
+function draw() {
+  frameRate(10);
+  background(220);
+  scale(1.5,1.5,0);
+  DrawMesh (6,6);
+}
+
+// Draw n superposed quadrangles (based on 40*40 squares)
+// Position i-column, j-row in the Mesh
+
+function DrawSquare(n, i, j) {
+  push(); 
+    noFill();
+    translate(60*i, 60*j, 0); // put it on i-column, j-row, with a 20px "distance" between 2 quads
+
+    for (let k=0; k<n; k++) {  // n times
+      let dispersion = [];
+
+      for (let d=0; d<8; d++) { // generate the dispersion array, filled with 8 random relative integers which will be the dispersions around the 8 coordinates
+        let disp = int (random(-12, 15));
+        dispersion.push(disp);
+      }
+      
+      
+// draw the quadrangle :
+      
+            if (s==1) {
+         stroke(20*(i+j),20*i,20*j); // color gradient
+
+      }
+      if (s==2) {
+         stroke(20*i,20*(i+j),20*j); // color gradient
+
+      }
+      if (s==3) {
+        stroke(20*i,20*i,20*(i+j)); // color gradient
+
+      }
+      
+     quad(40+dispersion[0],40+dispersion[1],80+dispersion[2],40+dispersion[3],80+dispersion[4],80+dispersion[5],40+dispersion[6],80+dispersion[7]);
+
+    }
+  
+  pop();
+}
+
+// Draw the i*j mesh of n superposed quadrangles
+function DrawMesh(w,h) {
+   let quad_amount;
+  strokeWeight(0.7);
+  
+ for (let j=0; j<h/2; j++) {
+      for (let i=w-1; i>=0; i--) {
+        ijquad_amount=50-DistIJ(i,j)/10; // increasing from right to left (-i), keep the number increasing from row to row (w*j), as in the original artwork (*2)
+        
+        // 3 first lines (brown)
+        //stroke("#8f746d");
+        DrawSquare(ijquad_amount,i,j);
+        
+      }
+  }
+  
+   for (let j=0; j<h/2; j++) {
+      for (let i=w-1; i>=0; i--) {
+        ijquad_amount=50-DistIJ(w-1-i, h-1-j)/10; // split in 2 the previous amount (/2) to draw half brown and half black
+    
+        // 3 last lines (brown)
+        //stroke("#8f746d");
+        DrawSquare(ijquad_amount, w-1-i, h-1-j); // positions = start bottom right
+        
+        // 3 last lines (black)
+        //stroke("#515757");
+        DrawSquare(ijquad_amount, w-1-i, h-1-j);
+    
+      }
+  }
+  
+}
+
+function DistIJ(i, j) {
+  let X = i*90 + 90;
+  let Y = j*90 + 90;
+  return sqrt((X-mouseX)*(X-mouseX) + (Y-mouseY)*(Y-mouseY));
+  
+}
+function mouseClicked() {
+  
+ s++; 
+  
+  if (s === 4) {
+    s = 1;
+  } 
+}
